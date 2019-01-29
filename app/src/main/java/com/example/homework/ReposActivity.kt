ca4a834aca421412b025bf5189a09953f36778d0
kimch3617 <kimch3617@gmail.com>
2019-01-29 16:37:20 +0900

    add app link

--- app/src/main/java/com/example/homework/ReposActivity.kt
+++ app/src/main/java/com/example/homework/ReposActivity.kt
@@ -1,25 +1,23 @@
 package com.example.homework
 
 import android.os.Bundle
-import android.os.Handler
 import android.support.v7.widget.LinearLayoutManager
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
 import com.example.homework.base.BaseActivity
 import com.example.homework.model.Repos
 import com.example.homework.model.User
 import kotlinx.android.synthetic.main.activity_repos.*
-import android.R.attr.data
 import java.util.*
-
 
 class ReposActivity : BaseActivity(), ReposView{
     private lateinit var reposAdapter: ReposAdapter
     private lateinit var presenter: ReposPresenter
+    private var username = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_repos)
+
+        val username = intent.data?.path ?: ""
 
         reposAdapter = ReposAdapter()
         recycler_repos.adapter = reposAdapter
@@ -27,11 +25,11 @@ class ReposActivity : BaseActivity(), ReposView{
 
         presenter = ReposPresenter()
         presenter.bindView(this)
-        presenter.getUsername("jakewharton")
+        presenter.getUsername(username)
     }
 
     override fun setUserProfile(user: User) {
-        presenter.getRepos("jakewharton")
+        presenter.getRepos(username)
         reposAdapter.addItem(user)
     }
 
